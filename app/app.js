6e1ac5fb809e2dd2d063bc53eb5799289bed3009
Hajime Taira <htaira@pantora.net>
2020-01-18 00:57:34 +0900

    Added initial code

---
+++ app/app.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012-2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+/*eslint-env node*/
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+/*
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
+ */
