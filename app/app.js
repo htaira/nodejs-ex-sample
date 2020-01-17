389172b933f4959debba5a1bc72fb4374fb0a67b
Hajime Taira <htaira@pantora.net>
2020-01-18 01:10:41 +0900

    Updated app/app.js

--- app/app.js
+++ app/app.js
@@ -21,12 +21,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-/*
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
- */
